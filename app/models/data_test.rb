cbc0556bec03ce987533dae5192b386d993ebf02
keisukeadachi <c3a578zx@gmail.com>
2019-06-06 21:48:32 +0900

    add csv_data

---
+++ app/models/data_test.rb
@@ -0,0 +1,2 @@
+class DataTest < ApplicationRecord
+end
---
+++ db/migrate/20190606123254_create_data_tests.rb
@@ -0,0 +1,10 @@
+class CreateDataTests < ActiveRecord::Migration[5.2]
+  def change
+    create_table :data_tests do |t|
+      t.string :name
+      t.integer :age
+      t.string :address
+      t.timestamps
+    end
+  end
+end
